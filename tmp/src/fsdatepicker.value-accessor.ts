ef71809d130b4222d2772da1c645bb7387a3e813
Anatoly <mendor1990@gmail.com>
2018-02-14 11:04:56 +0200

    GO-T67

--- tmp/src/fsdatepicker.value-accessor.ts
+++
@@ -1,9 +0,0 @@
-import { FsDatepickerDirective } from './fsdatepicker.directive';
-import { Provider, forwardRef } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
-
-export const DATEPICKER_VALUE_ACCESSOR: Provider = {
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => FsDatepickerDirective),
-  multi: true
-};
