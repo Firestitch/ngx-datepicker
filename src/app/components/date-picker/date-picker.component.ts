981e8d446f6d0d6e2b9de4f3803068459209743a
Eugene Kubesh <e.kubesh@wis.eu.com>
2019-11-04 13:30:08 +0300

    fixed change detection for datepicker and upgraded to 8.0.4

--- src/app/components/date-picker/date-picker.component.ts
+++ src/app/components/date-picker/date-picker.component.ts
@@ -79,6 +79,8 @@ export class FsDatePickerComponent extends FsDatePickerBaseComponent implements 
   public writeValue(value: any): void {
     this.ngModel = createDateFromValue(value);
     this.updateInput(value);
+
+    this._cdRef.markForCheck();
   }
 
   public updateInput(value) {
