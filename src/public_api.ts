fa951b50da271b12bc53065400ddd2266f49f48a
Eugene Kubesh <e.kubesh@wis.eu.com>
2020-08-12 17:30:48 +0300

    add exported components to public_api

--- src/public_api.ts
+++ src/public_api.ts
@@ -3,3 +3,11 @@
  */
 
 export { FsDatePickerModule } from './app/datepicker.module';
+
+export { FsDatePickerComponent } from './app/components/date-picker/date-picker.component';
+export { DateRangeSeparatorComponent } from './app/components/date-range-separator/date-range-separator.component';
+export { DateRangePickerToComponent } from './app/components/range-picker/to/range-picker-to.component';
+export { DateRangePickerFromComponent } from './app/components/range-picker/from/range-picker-from.component';
+export { FsDateScrollPickerComponent } from './app/components/date-scroll-picker/date-scroll-picker.component';
+export { FsDatePickerBirthdayComponent } from './app/components/date-picker-birthday/date-picker-birthday.component';
+export { FsDateWeekPickerComponent } from './app/components/date-week-picker/date-week-picker.component';
