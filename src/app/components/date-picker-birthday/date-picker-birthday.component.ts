d6a7391f7c8853f0d3d64ee7de70312af7b1e835
Eugene Kubesh <codbasters@gmail.com>
2021-11-22 15:53:06 +0300

    fix birthday picker

--- src/app/components/date-picker-birthday/date-picker-birthday.component.ts
+++ src/app/components/date-picker-birthday/date-picker-birthday.component.ts
@@ -1,7 +1,15 @@
-import { ChangeDetectionStrategy, Component, forwardRef, Input, } from '@angular/core';
+import {
+  ChangeDetectionStrategy, ChangeDetectorRef,
+  Component, ElementRef,
+  forwardRef, Inject,
+  Injector,
+  Input,
+  Renderer2,
+} from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { FsDatePickerComponent } from '../date-picker/date-picker.component';
 import { FsDateScrollPickerComponent } from '../date-scroll-picker/date-scroll-picker.component';
+import { FsDatePickerDialogFactory } from '../../../libs/dialog/services/dialog-factory.service';
 
 
 @Component({
@@ -21,4 +29,14 @@ export  class FsDatePickerBirthdayComponent extends FsDateScrollPickerComponent 
 
   @Input()
   public maxYear = (new Date()).getFullYear();
+
+  constructor(
+    renderer: Renderer2,
+    injector: Injector,
+    elementRef: ElementRef,
+    _datepickerFactory: FsDatePickerDialogFactory,
+    _cdRef: ChangeDetectorRef,
+  ) {
+    super(renderer, injector, elementRef, _datepickerFactory, _cdRef);
+  }
 }
